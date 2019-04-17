7ccd1cf1b4742365d7de6e720afe42c0d00b3798
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-04-16 22:00:03 -0400

    created server file

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/index.js');
+
+app.use(express.json());
+app.use('/', routes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});

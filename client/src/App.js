fab610a45b3b89b24688ef7dc376555309a481fd
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-04-19 14:39:29 -0400

    app.js homepage component, imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './components/HomePage.js';
+import axios from 'axios';
 
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/dogs" component={HomePage}/>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;

af2f92bdb0e1bf6ac6e89f30fe7bfb76abab6b30
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-04-19 16:18:43 -0400

    add new and edit routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomePage from './components/HomePage.js';
 import axios from 'axios';
+import HomePage from './components/HomePage.js';
+import NewDogForm from './components/NewDogForm.js';
+import EditDogForm from './components/EditDogForm.js';
 
 class App extends Component {
   render() {
@@ -9,6 +11,9 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/dogs" component={HomePage}/>
+          <Route path="/dogs/new" component={NewDogForm} />
+          <Route path="/dogs/:dogId/edit" component={EditDogForm} />
+          <Route />
         </Switch>
       </Router>
     );

4da09fbc3251286a895bc1a1c1e5d5c8aaf85ef6
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-04-19 15:50:06 -0400

    created mount component method

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -7,11 +7,26 @@ class HomePage extends Component {
     state = {
         dogs: [{}]
     };
+    componentDidMount = () => {
+        axios.get(`/dogs`)
+            .then(res => {
+                this.setState({
+                    dogs: res.data
+                });
+            });
+    }
     render() {
         return (
             <div>
                 <h1>PupTopia</h1>
-                <p></p>
+                <Link to="/dogs/new">Add A Dog</Link>
+                <ul>
+                    {this.state.dogs.map((dog) => (
+                        <li>
+                            <Link to={`/dogs/${dog._id}`}>{dog.name}</Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }

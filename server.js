ceda17a10377411195b94dda8b4d3840c2224062
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-04-20 09:41:12 -0400

    fixed client build issue

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const logger = require('morgan')
 const app = express();
 const routes = require('./routes/index.js');
 
+app.use(logger('dev'))
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
 app.use(express.json());
-app.use('/', routes);
+
+app.use('/api/v1', routes);
+
+app.use(express.static(`${__dirname}/client/build`))
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
